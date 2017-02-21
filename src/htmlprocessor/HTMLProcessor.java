9b99fd9df5c8f64c49e9b23e05726061b0837909
Andrea Serrano Urea <0andreasu@gmail.com>
2017-02-21 22:00:34 +0100

    Filter and Normalization finished.

---
+++ src/htmlprocessor/HTMLProcessor.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htmlprocessor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author Andrea
+ */
+public abstract class HTMLProcessor {
+    
+    /**
+     * Given an HTML file, it extracts its plain text
+     * @param path path of the file
+     * @return Plain text
+     * @throws IOException 
+     */
+    private static String filter(String path) throws IOException{
+        FileReader r = new FileReader(new File(path));
+        BufferedReader br = new BufferedReader(r);
+        
+        String line;
+        String text = "";
+        while ( (line=br.readLine()) != null) {
+            text += line;
+        }
+        
+        return Jsoup.parse(text).text();
+    }
+    
+    /**
+     * Given a text, this function returns the same text without capital letters,
+     * accents or strange characters. It also puts a '\n' between every word.
+     * @param text input text
+     * @return normalized text
+     */
+    private static String normalizeText(String text){
+        String outputText = "";
+        
+        String[] listOfWords = text.split(" ");
+        
+        int charCode;
+        
+        for (String word: listOfWords){
+            word = word.toLowerCase();
+            
+            word = StringUtils.stripAccents(word);        
+            
+            word = word.replaceAll("[^a-z0-9-_\\n]", "");
+            
+            outputText += word + '\n';
+        }
+        
+        
+        return outputText;
+    }
+
+    /**
+     * Given a file, it filters and normalizes it. It returns the normalized text.
+     * @param path path of the original file
+     */
+    public static String process(String path){
+
+        try {
+            String text = filter(path);
+            text = normalizeText(text);
+
+            return text;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+    
+}
--- src/sri/SRI.java
+++ src/sri/SRI.java
@@ -5,14 +5,16 @@
  */
 package sri;
 
-import htmlprocessor.HTMLFilter;
+import htmlprocessor.HTMLProcessor;
+
+import java.io.File;
 import java.io.IOException;
-import org.jsoup.examples.HtmlToPlainText;
-import org.jsoup.nodes.Attribute;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.helper.StringUtil;
+
 
 /**
  *
@@ -22,9 +24,67 @@ public class SRI {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
-        HTMLFilter.getText("documents/es_26142.html");
-        
+
+        long time_start, time_end;
+        String documentsPath = "documents";
+        Scanner keyboard = new Scanner(System.in);
+        String option;
+        int totalTokens = 0;
+        float tokensPerFile = 0;
+        int numFiles = 0;
+
+        /*******************************************************************************/
+
+
+        System.out.println("Starting HTML processor...");
+
+        do{
+            System.out.println("Do you want to process the default folder (./documents)? [Y/N] ");
+            option = keyboard.nextLine();
+        } while (!option.equals("Y") && !option.equals("y") && !option.equals("N") && !option.equals("n"));
+
+        if (option.equals("N") || option.equals("n")){
+            System.out.println("Enter the path of the folder: ");
+            documentsPath = keyboard.nextLine();
+        }
+
+        File documents = new File(documentsPath);
+        if (!documents.exists()){
+            System.out.println("The folder "+documentsPath+" doesn't exist. Exiting...");
+            System.exit(0);
+        }
+
+
+        time_start = System.currentTimeMillis();
+
+        File folder = new File("processed");
+        if (!folder.exists()) folder.mkdir();
+        else FileUtils.cleanDirectory(folder);
+
+
+
+        File[] listOfDocuments = documents.listFiles();
+        numFiles = listOfDocuments.length;
+
+        String text;
+        System.out.println("Processing documents...");
+        for (File file: listOfDocuments) {
+            text = HTMLProcessor.process(file.getPath());
+            File archive = new File(folder.getPath() + '/' + file.getName());
+            FileUtils.writeStringToFile(archive, text, "UTF-8");
+
+            totalTokens += StringUtils.countMatches(text,"\n");
+        }
+
+        time_end = System.currentTimeMillis() - time_start;
+        System.out.println("Processing finished. You can find the new files in ./processed folder. Exiting...");
+
+        tokensPerFile = (float) totalTokens / numFiles;
+        System.out.println("Total time of processing (including I/O operations): "+ time_end / 1000.0 + " seconds.");
+        System.out.printf("Total tokens obtained: %s\nAverage tokens per file: %s", totalTokens, tokensPerFile);
+
     }
 }

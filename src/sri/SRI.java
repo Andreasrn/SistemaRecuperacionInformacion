24f9693dbea7c06e53cc59ff6dba96c760cff2c3
Andrea Serrano Urea <0andreasu@gmail.com>
2017-04-12 11:40:32 +0200

    It shows on screen the sentence containing the words of the query (normalized and dirty)

--- src/sri/SRI.java
+++ src/sri/SRI.java
@@ -3,6 +3,7 @@
  * @Author: Andrea Serrano Urea
  */
 package sri;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -76,7 +77,8 @@ public class SRI {
 
                 for (int i = 0; i < Integer.parseInt(params.get(RELEVANT_DOCS)); i++){
                     doc = retrievedDocs.poll();
-                    printResult(i+1,doc,params.get(DOCUMENTS_FOLDER));
+                    printResult(i+1,doc,params.get(DOCUMENTS_FOLDER),query);
+
                 }
 
 
@@ -113,7 +115,7 @@ public class SRI {
      * @param document Pair containing the name of the doc and similarity with the query
      * @param path path where original files are stored
      */
-    private static void printResult(int num, Pair<String,Double> document, String path) throws IOException {
+    private static void printResult(int num, Pair<String,Double> document, String path, String query) throws IOException {
 
         FileReader r = new FileReader(new File(path+"/"+document.getFirst().replace(".txt",".html")));
         BufferedReader br = new BufferedReader(r);
@@ -132,7 +134,64 @@ public class SRI {
 
         System.out.printf("\t%s\n",html.title());
 
-        System.out.printf("\t<Frase que contiene la consulta>\n");
+        System.out.println();
+
+        System.out.printf("\t%s\n", lookForSentenceWhichContains(query.split("\\s"),"documents/"+document.getFirst().replace(".txt",".html")));
+
+    }
+
+    /**
+     * It returns the first ocurrence of a sentence containing words of the given query
+     * @param words array with the words of the query
+     * @param documentPath path of the document which contains some word of the query
+     * @return sentence containing max and first match
+     * @throws IOException
+     */
+    private static String lookForSentenceWhichContains(String[] words, String documentPath) throws IOException {
+
+        File document = new File(documentPath);
+
+        if (!document.exists()) throw new FileNotFoundException("File located at "+documentPath+" doesn't exist.\n");
+
+        FileReader r = new FileReader(document);
+        BufferedReader br = new BufferedReader(r);
+
+        String line;
+        String documentText = "";
+        while ( (line=br.readLine()) != null) {
+            documentText += line;
+        }
+
+        documentText = Jsoup.parse(documentText).text();
+
+        documentText = documentText.toLowerCase();
+
+        documentText = StringUtils.stripAccents(documentText);
+
+        documentText = documentText.replaceAll("[^a-z0-9-._\\n]", " ");
+
+        String[] listOfSentences = documentText.split("\\.");
+
+
+        int matches, maxMatches = 0;
+        String output = "";
+
+        for (String sentence: listOfSentences){
+
+            matches = 0;
+
+            for (String word: words){
+                if (sentence.contains(word)) matches++;
+            }
+
+            if (matches == words.length) return sentence;
+            if (matches > maxMatches){
+                maxMatches = matches;
+                output = sentence;
+            }
+        }
+
+        return output;
 
     }
 
